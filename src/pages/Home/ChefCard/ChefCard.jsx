e7100155752f7480208bdf70c1ed0f84ee356dc6
SimantaSarker <simantasarker0000@gmail.com>
2023-05-02 15:51:00 +0600

    View Recipes button added

--- src/pages/Home/ChefCard/ChefCard.jsx
+++ src/pages/Home/ChefCard/ChefCard.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 
 const ChefCard = ({ chef }) => {
-  const { chefName, chefPicture, likes, numberOfRecipes, yearsOfExperience,chefId } =
-    chef;
+  const {
+    chefName,
+    chefPicture,
+    likes,
+    numberOfRecipes,
+    yearsOfExperience,
+    chefId,
+  } = chef;
   return (
     <div className="card  bg-base-100 shadow-xl about">
       <figure className="h-4/5">
@@ -13,13 +19,18 @@ const ChefCard = ({ chef }) => {
           {chefName}
           <div className="badge badge-secondary">Likes: {likes}</div>
         </h2>
-        <p className="text-xl font-semibold">Years Of Experience: {yearsOfExperience}</p>
-        <div className="card-actions items-center justify-between mt-4">
-          <div className="badge badge-primary badge-outline badge-lg cursor-pointer text-xl p-3">
+        {/* <p className="text-xl font-semibold"> Experience: {yearsOfExperience}</p> */}
+        <div className="card-actions  mt-4">
+          <div className="badge  badge-outline badge-lg cursor-pointer text-xl p-3">
             Number of Recipes : {numberOfRecipes}
           </div>
-          <div className="badge badge-primary badge-outline badge-lg cursor-pointer text-xl p-4">
-            Likes:{likes}
+          <div className="badge  badge-outline badge-lg cursor-pointer text-xl p-3">
+            Experience: {yearsOfExperience}
+          </div>
+          <div className="text-start mt-4 ">
+            <button className="btn btn-outline btn-secondary ">
+              View Recipes
+            </button>
           </div>
         </div>
       </div>
