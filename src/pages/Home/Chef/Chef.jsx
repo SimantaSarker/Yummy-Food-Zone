a2320dc8ad6c3dfcd58c0b1c2a81bdd637069d9a
SimantaSarker <simantasarker0000@gmail.com>
2023-05-02 15:39:45 +0600

    chef heading part added

--- src/pages/Home/Chef/Chef.jsx
+++ src/pages/Home/Chef/Chef.jsx
@@ -1,7 +1,7 @@
 import { data } from 'autoprefixer';
 import React, { useEffect, useState } from 'react';
 import ChefCard from '../ChefCard/ChefCard';
-
+import './Chef.css'
 const Chef = () => {
 
   const [chefs,setChefs]=useState([]);
@@ -11,14 +11,21 @@ const Chef = () => {
     .then((data)=>setChefs(data))
   },[])
 
-  console.log(chefs)
 
   return (
-    <div className='grid md:grid-cols-3 lg:grid-cols-3 gap-4 mb-8 m-auto p-2 '>
+  <div>
+    <div className='chef text-center'>
+      <p className='text-2xl mt-3 mb-4 text-blue-600'>Our Team Of Professionals </p>
+    <h1 className='text-5xl'>Meet Our Expert Chefs,We Love Our Chefs</h1>
+
+    </div>
+    <hr className='border-4 border-blue-200 w-1/2 mx-auto mb-9 mt-6' />
+      <div className='grid md:grid-cols-3 lg:grid-cols-3 gap-4 mb-8 m-auto p-2 '>
       {
         chefs.map((chef)=><ChefCard key={chef.chefId} chef={chef}></ChefCard>)
       }
     </div>
+  </div>
   );
 };
 
--- src/pages/Home/ChefCard/ChefCard.jsx
+++ src/pages/Home/ChefCard/ChefCard.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 
 const ChefCard = ({ chef }) => {
-  const { chefName, chefPicture, likes, numberOfRecipes } = chef;
+  const { chefName, chefPicture, likes, numberOfRecipes, yearsOfExperience,chefId } =
+    chef;
   return (
-    <div className="card  bg-base-100 shadow-xl">
-      <figure>
-        <img
-          src={chefPicture}
-          alt="Shoes" className="w-full"
-        />
+    <div className="card  bg-base-100 shadow-xl about">
+      <figure className="h-4/5">
+        <img src={chefPicture} alt="Shoes" className="h-full rounded " />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">
-          Shoes!
-          <div className="badge badge-secondary">NEW</div>
+        <h2 className="card-title font-semibold text-3xl">
+          {chefName}
+          <div className="badge badge-secondary">Likes: {likes}</div>
         </h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <div className="badge badge-outline">Fashion</div>
-          <div className="badge badge-outline">Products</div>
+        <p className="text-xl font-semibold">Years Of Experience: {yearsOfExperience}</p>
+        <div className="card-actions items-center justify-between mt-4">
+          <div className="badge badge-primary badge-outline badge-lg cursor-pointer text-xl p-3">
+            Number of Recipes : {numberOfRecipes}
+          </div>
+          <div className="badge badge-primary badge-outline badge-lg cursor-pointer text-xl p-4">
+            Likes:{likes}
+          </div>
         </div>
       </div>
     </div>
