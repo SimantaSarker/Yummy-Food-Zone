8287b34a496ea7f731fec99a2febd270676b3a60
SimantaSarker <simantasarker0000@gmail.com>
2023-05-02 19:31:43 +0600

    ingredients,favorite,ratings added in single recipe

--- src/pages/Home/ChefRecipe/SingleRecipe.jsx
+++ src/pages/Home/ChefRecipe/SingleRecipe.jsx
@@ -2,13 +2,30 @@ import React from "react";
 
 const SingleRecipe = ({ recipe }) => {
   console.log(recipe);
-const {cookingMethod,rating,recipeName,ingredients}=recipe;
-console.log(cookingMethod,rating,recipeName,ingredients);
+  const { cookingMethod, rating, recipeName, ingredients } = recipe;
+  console.log(ingredients);
   return (
-    <div className="card w-9/12  bg-gray-600 text-primary-content mx-auto">
+    <div className="card w-9/12  bg-gray-600 text-primary-content mx-auto about">
       <div className="card-body">
-        <h2 className="card-title">Recipe Name: {recipeName}</h2>
+        <h2 className="card-title text-2xl">Recipe Name: {recipeName}</h2>
+        <hr className="border-2 border-black mt-4 mb-4 rounded " />
         <p>{cookingMethod}</p>
+        <hr className="border-2 border-black mt-4 mb-4 rounded " />
+        <div className="flex justify-between items-center">
+          <div>
+            <h1 className="text-slate-300 font-semibold text-2xl">
+              INGREDIENTS:
+            </h1>
+            <ul>
+              <li>1. {ingredients.ingredients_1}</li>
+              <li>2. {ingredients.ingredients_2}</li>
+              <li>3. {ingredients.ingredients_3}</li>
+            </ul>
+          </div>
+          <div>
+            <p>Ratings: {rating}</p>
+          </div>
+        </div>
         <div className="card-actions justify-end">
           <button className="btn btn-success">Favorite</button>
         </div>
