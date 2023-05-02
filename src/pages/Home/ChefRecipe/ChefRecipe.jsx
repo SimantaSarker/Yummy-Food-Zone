fa93f967f39242e164e70fa88bc7ee5f1199b8b6
SimantaSarker <simantasarker0000@gmail.com>
2023-05-02 16:35:36 +0600

    fetech single chef details

--- src/pages/Home/ChefRecipe/ChefRecipe.jsx
+++ src/pages/Home/ChefRecipe/ChefRecipe.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useLoaderData, useParams } from 'react-router-dom';
 
 const ChefRecipe = () => {
+  const chefRecipes=useLoaderData();
+  const {id}=useParams();
+  console.log(id)
+  console.log(chefRecipes)
   return (
     <div>
-      <h1>Chef Recipe
+      <h1>Chef Recipe id {id}
       </h1>
     </div>
   );
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,43 +1,36 @@
-import {
-  createBrowserRouter,
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 
 import Main from "../layouts/Main";
 import Home from "../pages/Home/Home/Home";
 import Blog from "../pages/Home/Blog/Blog";
-import BlogLayout from "../layouts/BlogLayout";
 import ChefRecipeLayout from "../layouts/ChefRecipeLayout";
 import ChefRecipe from "../pages/Home/ChefRecipe/ChefRecipe";
-const router=createBrowserRouter([
+const router = createBrowserRouter([
   {
-    path:'/',
-    element:<Main></Main>,
-    children:[
+    path: "/",
+    element: <Main></Main>,
+    children: [
       {
-        path:'/',
-        element:<Home></Home>,
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "blog",
+        element: <Blog></Blog>,
       },
     ],
   },
+ 
   {
-    path:'blog',
-    element:<BlogLayout></BlogLayout>,
-    children:[
+    path: "chefRecipe",
+    element: <ChefRecipeLayout></ChefRecipeLayout>,
+    children: [
       {
-        path:'/blog',
-        element:<Blog></Blog>
-      }
-    ]
+        path: ":id",
+        element: <ChefRecipe></ChefRecipe>,
+        loader:({params})=>fetch(`http://localhost:4000/chefs/${params.id}`)
+      },
+    ],
   },
-  {
-    path:'chefRecipe',
-    element:<ChefRecipeLayout></ChefRecipeLayout>,
-    children:[
-      {
-        path:':id',
-        element:<ChefRecipe></ChefRecipe>
-      }
-    ]
-  }
-])
+]);
 export default router;
