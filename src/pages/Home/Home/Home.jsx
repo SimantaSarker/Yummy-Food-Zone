1f398704b0f020ab59df8dd62aed1b97c1666f1f
SimantaSarker <simantasarker0000@gmail.com>
2023-05-04 21:51:43 +0600

    lazy loader added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React,{Suspense,lazy} from 'react';
+
 import './Home.css'
 import Banner from '../Banner/Banner';
 import AboutUs from '../AboutUs/AboutUs';
-import Chef from '../Chef/Chef';
+// import Chef from '../Chef/Chef';
 import SpecialWeek from '../SpecialWeek/SpecialWeek';
+const Chef=lazy(()=>import('../Chef/Chef'))
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <AboutUs></AboutUs>
-      <Chef></Chef>
+     <Suspense fallback={<div><progress className="progress w-56"></progress></div>}> <Chef></Chef></Suspense>
       <SpecialWeek></SpecialWeek>
     </div>
   );
